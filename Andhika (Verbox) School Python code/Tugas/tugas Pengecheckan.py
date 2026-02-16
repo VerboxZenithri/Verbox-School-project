482db0154a309e3f46161814d7784a6058c8c172
VerboxZenithri <shafieandhika@gmail.com>
2026-02-16 21:57:57 +0700

    Update tugas Pengecheckan.py

--- Andhika (Verbox) School Python code/Tugas/tugas Pengecheckan.py
+++ Andhika (Verbox) School Python code/Tugas/tugas Pengecheckan.py
@@ -1,12 +1,12 @@
-kelipatan_1 = 4
-kelipatan_2 = 6 
+kelipatan_1=4
+kelipatan_2=6 
 kata_1 = "pertanyaan"  #Kata untuk Aturan 1
 kata_2 = "tanya pertanyaan"  #Kata untuk Aturan 2
-kata_gabungan = "kebanyakan tanya!!!"
+kata_gabungan="kebanyakan tanya!!!"
 #Melakukan pengecekan
 for angka in range(1, 91):
-    memenuhi_aturan_1 = (angka % kelipatan_1 == 0)
-    memenuhi_aturan_2 = (angka % kelipatan_2 == 0)
+    memenuhi_aturan_1=(angka % kelipatan_1 == 0)
+    memenuhi_aturan_2=(angka % kelipatan_2 == 0)
     if memenuhi_aturan_1 and memenuhi_aturan_2:
         print(kata_gabungan)
     elif memenuhi_aturan_1 and not memenuhi_aturan_2:
